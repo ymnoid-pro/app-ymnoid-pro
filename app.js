97c137be42b2f2e2fdcb8c3452a57809d51a2601
ymnoid-pro <141595977+ymnoid-pro@users.noreply.github.com>
2023-08-12 03:35:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", showCoffees);
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("https://raw.githubusercontent.com/ymnoid-pro/app-ymnoid-pro/main/serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
